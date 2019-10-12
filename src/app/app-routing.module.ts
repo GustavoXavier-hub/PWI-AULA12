0b97b0566ac0ca78b9e6a563c7c63e8f59215f2d
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-10-11 22:21:18 -0300

    pwIII-Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProdutosComponent} from  './produtos/produtos.component';
+import {NovoProdutoComponent} from './novo-produto/novo-produto.component';
+import {ContentComponent} from './content/content.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path :'',
+    component : ContentComponent 
+    },
+
+{
+path :'produtos',
+component : ProdutosComponent 
+},
+{
+  path :'novosprodutos',
+  component : NovoProdutoComponent
+  }
+  
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
